e0589d1a72ab84b26ef3cf1dff9f1fb0b78ca1bd
Adeyoriju Olabode Wilson <whilson03@gmail.com>
2020-04-02 10:20:57 +0100

    deleteListener to remove a selected label item from a note

--- app/src/main/java/com/olabode/wilson/daggernoteapp/adapters/LabelChipListAdapter.kt
+++ app/src/main/java/com/olabode/wilson/daggernoteapp/adapters/LabelChipListAdapter.kt
@@ -1,6 +1,5 @@
 package com.olabode.wilson.daggernoteapp.adapters
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
@@ -12,8 +11,18 @@ import com.olabode.wilson.daggernoteapp.models.Label
 /**
  *   Created by OLABODE WILSON on 2020-04-02.
  */
-class LabelChipListAdapter(val context: Context) : ListAdapter<Label,
+class LabelChipListAdapter : ListAdapter<Label,
         LabelChipListAdapter.LabelChipViewHolder>(LabelDiffCallBack()) {
+
+    private var deleteListener: OnItemDeleteClickListener? = null
+
+    interface OnItemDeleteClickListener {
+        fun onDeleteClicked(Label: Label)
+    }
+
+    fun setDeleteClickListener(listener: OnItemDeleteClickListener) {
+        this.deleteListener = listener
+    }
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LabelChipViewHolder {
@@ -29,11 +38,16 @@ class LabelChipListAdapter(val context: Context) : ListAdapter<Label,
         holder.bind(item)
     }
 
-
     inner class LabelChipViewHolder constructor(val binding: ItemLabelChipBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(item: Label) {
             binding.label = item
+            binding.removeKeywordButton.setOnClickListener {
+                val position = adapterPosition
+                if (deleteListener != null && position != RecyclerView.NO_POSITION) {
+                    deleteListener!!.onDeleteClicked(getItem(position))
+                }
+            }
             binding.executePendingBindings()
         }
     }
